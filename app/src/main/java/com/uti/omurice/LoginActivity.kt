9beeb8da6d2673598adb4ecf065f4ed75293cfcc
yogzism890 <yogznovachrono@email.com>
2025-05-08 20:59:50 +0700

    Initial commit

---
+++ app/src/main/java/com/uti/omurice/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.uti.omurice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uti.omurice.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        openfragmentLogin()
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun openfragmentLogin(){
+supportFragmentManager.beginTransaction().replace(R.id.frame_layout_login, LoginFragment()).commit()
+    }
+}
---
+++ app/src/main/java/com/uti/omurice/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.uti.omurice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uti.omurice.databinding.ActivityMainBinding
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java )
+            startActivity(intent)
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
