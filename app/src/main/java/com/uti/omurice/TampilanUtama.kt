c0c1aadd9f1ab1c58d7d2347e61c0db375f54ca2
yogzism890 <yogznovachrono@email.com>
2025-05-09 22:45:18 +0700

    PROSES PEMBUATAN TAMPILAN UTAMA

---
+++ app/src/main/java/com/uti/omurice/TampilanUtama.kt
@@ -0,0 +1,31 @@
+package com.uti.omurice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uti.omurice.databinding.ActivityLoginBinding
+import com.uti.omurice.databinding.TampilanUtamaBinding
+
+class TampilanUtama : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+//        memanggil fungsi binding
+        val binding = TampilanUtamaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
