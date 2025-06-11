3f75f9098781a3eb759ca62d0a4aa5225475f0a2
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-06-11 20:45:57 +0700

    Tambah logika tombol checkout agar ketika di klik akan membuka CheckoutFragment

--- app/src/main/java/com/uti/omurice/CartFragment.kt
+++ app/src/main/java/com/uti/omurice/CartFragment.kt
@@ -6,19 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,11 +23,11 @@ class CartFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
 
-        // Handle tombol back di fragment ini
+        // Tangani tombol back untuk kembali ke FavoritFragment
         requireActivity().onBackPressedDispatcher.addCallback(this) {
-            // Navigasi balik ke FavoriteFragment
             parentFragmentManager.beginTransaction()
-                .replace(R.id.btn_back, FavoritFragment())
+                .replace(R.id.fragment_container, FavoritFragment()) // <-- ganti btn_back ke id container
+                .addToBackStack(null)
                 .commit()
         }
     }
@@ -42,21 +36,23 @@ class CartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        val view = inflater.inflate(R.layout.fragment_cart, container, false)
+
+        // Temukan tombol checkout
+        val checkoutButton: Button = view.findViewById(R.id.checkoutButton)
+
+        // Ketika tombol checkout diklik, ganti fragment
+        checkoutButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, CheckoutFragment())
+                .addToBackStack(null) // agar bisa back ke cart
+                .commit()
+        }
+
+        return view
     }
 
-
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             CartFragment().apply {
