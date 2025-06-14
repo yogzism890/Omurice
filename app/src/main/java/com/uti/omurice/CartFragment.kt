8f36ed0f85c3554d469801534d16eef99decb17b
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-06-14 15:41:50 +0700

    update logika button checkout agar ketika diklik bisa membuka/menampilkan fragment_checkout.xml

--- app/src/main/java/com/uti/omurice/CartFragment.kt
+++ app/src/main/java/com/uti/omurice/CartFragment.kt
@@ -8,25 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class CartFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
 
-        // Tangani tombol back untuk kembali ke FavoritFragment
+        // Tangani tombol back (fisik) untuk kembali ke FavoritFragment
         requireActivity().onBackPressedDispatcher.addCallback(this) {
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, FavoritFragment()) // <-- ganti btn_back ke id container
+                .replace(R.id.fragment_container, FavoritFragment())
                 .addToBackStack(null)
                 .commit()
         }
@@ -41,26 +31,14 @@ class CartFragment : Fragment() {
         // Temukan tombol checkout
         val checkoutButton: Button = view.findViewById(R.id.checkoutButton)
 
-        // Ketika tombol checkout diklik, ganti fragment
+        // Ketika tombol checkout diklik, ganti fragment ke CheckoutFragment
         checkoutButton.setOnClickListener {
             parentFragmentManager.beginTransaction()
-//                .replace(R.id.fragment_container, CheckoutFragment())
-                .addToBackStack(null) // agar bisa back ke cart
+                .replace(R.id.fragment_container, CheckoutFragment())
+                .addToBackStack(null) // agar bisa kembali ke Cart
                 .commit()
         }
 
         return view
-    }
-
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
