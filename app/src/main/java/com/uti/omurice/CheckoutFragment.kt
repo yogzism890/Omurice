76d82953d0c5d07c287fcd3b46591ddfeab0979c
Nazila Erza Hakim <nazila_erza_hakim@teknokrat.ac.id>
2025-06-18 16:33:11 +0700

    Menambahkan logika pada btnPay agar ketika diklik tombol trsbut akan menampilkan fragment_payment.xml

--- app/src/main/java/com/uti/omurice/CheckoutFragment.kt
+++ app/src/main/java/com/uti/omurice/CheckoutFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class CheckoutFragment : Fragment() {
 
@@ -25,6 +27,19 @@ class CheckoutFragment : Fragment() {
                 .replace(R.id.fragment_container, CartFragment())
                 .addToBackStack(null)
                 .commit()
+        }
+//        Membuka fragment pay jika btnpay di klik
+        val btnPay = view.findViewById<Button>(R.id.btnPay)
+
+        btnPay.setOnClickListener {
+            val fragmentPayment = PaymentFragment()
+
+            parentFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragmentPayment)
+                .commit()
+
         }
     }
 }
