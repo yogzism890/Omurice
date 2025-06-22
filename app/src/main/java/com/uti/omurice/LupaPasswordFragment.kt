8ccecafde504003e7e205ab4f130330aff0ecbdd
yogzism890 <yogznovachrono@gmail.com>
2025-06-23 00:20:13 +0700

    menambahkan logika pada radio check pada fragment lupa password

--- app/src/main/java/com/uti/omurice/LupaPasswordFragment.kt
+++ app/src/main/java/com/uti/omurice/LupaPasswordFragment.kt
@@ -1,59 +1,44 @@
 package com.uti.omurice
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.uti.omurice.databinding.FragmentLupaPasswordBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LupaPasswordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LupaPasswordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentLupaPasswordBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lupa_password, container, false)
+    ): View {
+        _binding = FragmentLupaPasswordBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Default: Message selected
+        binding.radioMsg.setImageResource(R.drawable.ic_radio_checked)
+        binding.radioEmail.setImageResource(R.drawable.ic_radio_unchecked)
+
+        // Logika Klik
+        binding.optionMessage.setOnClickListener {
+            binding.radioMsg.setImageResource(R.drawable.ic_radio_checked)
+            binding.radioEmail.setImageResource(R.drawable.ic_radio_unchecked)
+        }
+
+        binding.optionEmail.setOnClickListener {
+            binding.radioMsg.setImageResource(R.drawable.ic_radio_unchecked)
+            binding.radioEmail.setImageResource(R.drawable.ic_radio_checked)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LupaPasswordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LupaPasswordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
